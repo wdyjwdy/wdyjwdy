87fa03690d9d55d9a2de72ccfbb24aa6458fef12
wdyjwdy <46589632+wdyjwdy@users.noreply.github.com>
2023-10-23 03:01:28 +0800

    add

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import logo from './images/logo.png'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: (
+    <>
+      <img src={logo.src} width={40} height={40} alt='logo'/>
+      <span style={{fontWeight: 800, paddingLeft: '10px'}}>wdyjwdy</span>
+    </>
+  ),
   footer: {
-    text: 'Nextra Docs Template',
+    component: false
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s'
+    }
+  },
+  project: {
+    link: 'https://github.com/wdyjwdy/wdyjwdy',
+  },
+  docsRepositoryBase: 'https://github.com/wdyjwdy/wdyjwdy',
 }
 
 export default config
