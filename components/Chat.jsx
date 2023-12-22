923eecd080b88b2a08afd76e5c1b1fcb213851f8
wdyjwdy <46589632+wdyjwdy@users.noreply.github.com>
2023-12-23 07:31:51 +0800

    ...

---
+++ components/Chat.jsx
@@ -0,0 +1,30 @@
+import styles from './Chat.module.css'
+import xxc from '/images/xxc.jpg'
+import wj from '/images/wj.jpg'
+
+export default function Chat({ children }) {
+  return (
+    <div className={styles.chat}>
+      { children }
+    </div>
+  )
+}
+
+Chat.Item = Item
+
+export function Item({ name, text, right }) {
+  const textColor = name === 'wj' ? '#005493' : 'pink'
+  const avatar = name === 'wj' ? wj.src : xxc.src
+
+  return (
+    <div className={styles.item} style={{ flexDirection: right && 'row-reverse' }}>
+      <div className={styles.avatar}>
+        <img src={avatar} />
+      </div>
+      <div className={styles.message}>
+        <span style={{ alignSelf: right && 'end' }}>{ name }</span>
+        <p style={{ color: textColor, alignSelf: right && 'end' }}>{ text }</p>
+      </div>
+    </div>
+  )
+}
