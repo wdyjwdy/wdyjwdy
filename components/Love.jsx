8f6e9c08fdc759755e5a3e5f7e28734cfa4c26bc
wdyjwdy <46589632+wdyjwdy@users.noreply.github.com>
2023-12-23 23:47:26 +0800

    add Love widget

---
+++ components/Love.jsx
@@ -0,0 +1,11 @@
+import styles from './Love.module.css'
+
+export default function Love() {
+  return (
+    <div className={styles.love}>
+      <p>♥︎</p>
+      <p>WJ</p>
+      <p>XXC</p>
+    </div>
+  )
+}
