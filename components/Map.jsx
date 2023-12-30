1162ef59c5a39f66b776b0bdd04cef313273cc9f
wdyjwdy <46589632+wdyjwdy@users.noreply.github.com>
2023-12-30 20:43:54 +0800

    组件化

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,26 +1,71 @@
 import styles from './Map.module.sass'
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 import { chinaProvinces } from '../datas/maps'
 
+const items = [
+   { id: 1, text: '去过', color: 'indianred' },
+   { id: 2, text: '没去过', color: 'skyblue' },
+]
+
 export default function Map({ selected = [] }) {
-   const [pos, setPos] = useState([-100, -100])
+   const [pos, setPos] = useState({ x: -100, y: -100 })
    const [province, setProvince] = useState('')
    const handleMove = e => {
       setProvince(e.target.id)
-      setPos([e.pageX + 10, e.pageY + 20])
+      setPos({
+         x: e.pageX + 10,
+         y: e.pageY + 20
+      })
    }
 
    return (
       <div className={styles.map}>
-         <span className='toolTips' style={{ top: pos[1], left: pos[0] }}>{province}</span>
-         <svg xmlns="http://www.w3.org/2000/svg" viewBox="-30 -30 841.9 595.3" onMouseMove={handleMove}>
-            <rect x="-10" y='440' width="150" height="80" rx="15" />
-            <circle cx="10" cy="460" r="10" fill="indianred" />
-            <text x='30' y='470'>去过</text>
-            <circle cx="10" cy="500" r="10" fill="skyblue" />
-            <text x='30' y='510'>没去过</text>
-            {chinaProvinces.map(([id, d]) => <path d={d} id={id} className={selected.includes(id) ? 'selected' : ''} key={id} />)}
+         <ToolTip pos={pos} text={province} />
+         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 600" onMouseMove={handleMove}>
+            <Legend items={items} x={10} y={450} />
+            {chinaProvinces.map(([id, d]) => (
+               <path
+                  d={d} id={id} key={id}
+                  className={selected.includes(id) ? 'selected' : ''}
+               />
+            ))}
          </svg>
       </div>
+   )
+}
+
+function ToolTip({ pos, text }) {
+   return (
+      <span className={styles.tooltip} style={{ top: pos.y, left: pos.x }}>
+         {text}
+      </span>
+   )
+}
+
+function Legend({ items, x=10, y=10 }) {
+   // 需要在 <svg> 里使用
+   // items = [{ id: 1, text: '去过', color: 'indianred' }, ...]
+
+   const height = 40 * items.length
+
+   function Item({ text, color, x, y, index }) {
+      return (
+         <g className={`item-${index}`}>
+            <circle cx={x + 20} cy={y + 20} r={10} fill={color} />
+            <text x={x + 40} y={y + 30}>{text}</text>
+         </g>
+      )
+   }
+
+   return (
+      <g className={styles.legend}>
+         <rect x={x} y={y} width={150} height={height} rx={15} />
+         {items.map((item, index) => (
+            <Item
+               key={item.id} index={index} text={item.text}
+               color={item.color} x={x} y={y + index * 40}
+            />
+         ))}
+      </g>
    )
 }
