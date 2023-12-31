8fe993f001ad4ab9c0200e11f3189d4c1372691c
wdyjwdy <46589632+wdyjwdy@users.noreply.github.com>
2024-01-01 01:16:41 +0800

    简化组件导入

---
+++ components/index.js
@@ -0,0 +1,13 @@
+import Chat from './Chat'
+import Love from './Love'
+import Map from './Map'
+import Timer from './Timer'
+import Todo from './Todo'
+
+export {
+  Chat,
+  Love,
+  Map,
+  Timer,
+  Todo,
+}
